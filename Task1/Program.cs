fbfacc32c19d21212191350906602f2897df6122
Konstantin Chern <codahl@yandex.ru>
2023-03-27 01:11:10 +0300

    Task_1 solved

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,5 +4,24 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.Clear();
+int number = InputInt("Введите число N, до которого получаем квадраты: ");  
+int InputInt(string message)                             
+{
+    System.Console.Write($"{message}> ");                 
+    int value;                                            
+    if (int.TryParse(Console.ReadLine(), out value))      
+    {                                                      
+        return value;                                      
+    }                                                      
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);                                  
+    return 0;                                                
+}
 
+for (int i = 1; i <= number; i++)   
+{
+    int square = i * i;              
+    System.Console.Write($"{square} ");
+} 
 
