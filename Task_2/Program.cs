a4b904341f7ab646d12000a3906d71f4e0c64003
Konstantin Chern <codahl@yandex.ru>
2023-03-27 02:17:55 +0300

    Task_2 solved

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -6,4 +6,34 @@
 // Пример: ["hello", "2", "world", ";-)"]  =>  ["2", ";-)"]
 
 
+Console.Clear();
+
+string[] arrayOne = new string[7] {"Москва", "ОН", "Платон", "не", "007", "Phizik", "!!"};
+Console.Write($"{arrayOne} ==> ");
+string[] arrayTwo = new string[arrayOne.Length];
+
+
+int count = 0;
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+      {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
 
